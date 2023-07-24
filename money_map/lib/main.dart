35a8d2cf8d00b8840ab4536d9240fe63d7924963
SameelPookkodan <pookkodansameel@gmail.com>
2023-07-24 09:11:12 +0530

    last commit

--- money_map/lib/main.dart
+++ money_map/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:money_management_app/models/category/category_model.dart';
@@ -24,20 +23,17 @@ Future<void> main() async {
 
   runApp(
     
-    DevicePreview(
-      enabled: true,
-      builder:(context) =>const MyApp(),));
+    const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      title: 'Flutter Demo',
-      home: const SplashScreen(),
+    return const MaterialApp(
+     
+      title: 'Money Map',
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
